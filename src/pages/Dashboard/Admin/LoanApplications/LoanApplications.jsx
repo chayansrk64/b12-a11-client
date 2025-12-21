8e033ea98e9c5b6c7b7062b04546ad8b40ec25cc
chayansrk64 <chayansrk64@gmail.com>
2025-12-22 01:01:02 +0600

    loan application modified

--- src/pages/Dashboard/Admin/LoanApplications/LoanApplications.jsx
+++ src/pages/Dashboard/Admin/LoanApplications/LoanApplications.jsx
@@ -1,14 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import LoadingSpinner from '../../../../components/LoadingSpinner/LoadingSpinner';
 import { FaRegEye } from 'react-icons/fa6';
 import SectionTitle from '../../../../components/SectionTitle/SectionTitle';
+import { Link } from 'react-router';
 
 const LoanApplications = () => {
 
-    const modalRef = useRef(null);
-    const [selectedLoan, setSelectedLoan] = useState(null);
     const [statusFilter, setStatusFilter] = useState('all');
 
      const axiosSecure = useAxiosSecure()
@@ -24,11 +23,7 @@ const LoanApplications = () => {
     const filteredLoans = statusFilter === 'all' ? loans : loans.filter(loan => loan.status === statusFilter)
 
 
-    const handleShowModal = (loan) => {
-        setSelectedLoan(loan)
-        modalRef.current.showModal()
-    }
-
+    
 
      if(isPending){
          return <LoadingSpinner></LoadingSpinner>
@@ -82,65 +77,12 @@ const LoanApplications = () => {
                <td>{loan.loanAmount}</td>
                <td>{loan.status}</td>
                <td>
-                   <button onClick={() => handleShowModal(loan)} className='btn btn-sm hover:bg-green-500 hover:text-white'><FaRegEye /></button>
+                   <Link to={`/loan-details/${loan.loanId}`}  className='btn btn-sm hover:bg-green-500 hover:text-white'><FaRegEye /></Link>
                </td>
              </tr>)}
              
            </tbody>
          </table>
-
-
-{/* modal  */}
-{/* Open the modal using document.getElementById('ID').showModal() method */}
-{/* <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>open modal</button> */}
-<dialog ref={modalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-  <div className="modal-box">
-    
-    <h3 className="font-bold text-lg">Loan Details!</h3>
-   
-    {
-        selectedLoan && 
-        <>
-         <div className="hero bg-base-200 ">
-  <div className="hero-content flex-col">
-    <img
-      src={selectedLoan.image}
-      className=" rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 className="text-3xl font-bold mb-4">{selectedLoan.loanTitle}</h1>
-
-      <p>Loan Amount $ {selectedLoan.loanAmount}</p>
-      <p>Interest Rate: {selectedLoan.interestRate}</p>
-      <p>Interest Rate: {selectedLoan.interestRate}</p>
-      <p>applicants Email: {selectedLoan.applicantsEmail}</p>
-      <p>applicants Name: {selectedLoan.fName} {selectedLoan.lName}</p>
-      <p>applicants Contact: {selectedLoan.contact}</p>
-      <p>income Source: {selectedLoan.incomeSource}</p>
-      <p>monthly Income: {selectedLoan.monthlyIncome}</p>
-      <p>reason For Loan: {selectedLoan.reasonForLoan}</p>
-      <p>address: {selectedLoan.address}</p>
-      <p>status: {selectedLoan.status}</p>
-      <p>application FeeS tatus: {selectedLoan.applicationFeeStatus}</p>
-      
-    </div>
-  </div>
-</div>
-
-
-        
-        </>
-    }
-
-
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn">Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
 
 
        </div>
