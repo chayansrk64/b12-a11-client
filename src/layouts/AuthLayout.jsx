c0b8226d4912b3e0f9796de1a274751eff917e2d
chayansrk64 <chayansrk64@gmail.com>
2025-12-21 22:20:47 +0600

    authlayout  updated

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Outlet } from "react-router";
 import Logo from "../components/Logo/Logo";
 import authImg from '../assets/login-img.png'
+import Navbar from "../pages/Shared/Header/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const AuthLayout = () => {
   return (
-   <div className="max-w-7xl mx-auto min-h-screen flex flex-col bg-white">
-   <div className="ms-10 ">
-    <Logo/>
-   </div>
+  <>
+    <Navbar></Navbar>
+   <div className="max-w-7xl mx-auto  flex flex-col bg-white">
+   
 
   <div className="flex-1 flex ">
 
     {/* LEFT SIDE – FORM */}
     <div className="flex-1 flex justify-center items-center">
       <div className="w-full max-w-md px-6">
+      
         <Outlet />
+        
       </div>
     </div>
 
@@ -24,12 +28,14 @@ const AuthLayout = () => {
       <img
         src={authImg}
         alt="auth illustration"
-       className="w-full h-full object-cover"
+       className="w-full object-cover"
       />
     </div>
 
   </div>
 </div>
+<Footer></Footer>
+</>
 
   );
 };
